d94bbc2d2d375ea4026d54e2b97eb02b17494536
Janik Daub <daub.j@icloud.com>
2025-11-05 14:26:09 +0100

    Spring Initialization with Spring Web, Validation and Thymeleaf

---
+++ src/test/java/com/awesome/thesis/ThesisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awesome.thesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
