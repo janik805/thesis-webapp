c806fdab57efc6635d286a82551bc22ee96be223
Ryota Kariya <qet71seq@hhu.de>
2025-11-07 21:23:00 +0100

    Fuegt Klasse Links hinzu Durch diesen COmmit wird die Klasse Links mit Parameter Url und Text hinzugefügt. Die Klasse hat für beide Parameter jeweils Getter und Setter.

---
+++ src/main/java/com/awesome/thesis/Links.java
@@ -0,0 +1,22 @@
+package com.awesome.thesis;
+
+public class Links {
+    private String url;
+    private String text;
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
