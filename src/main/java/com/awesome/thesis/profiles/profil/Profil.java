aa5775d3895eb5740212d7483dd8ca98a038ee38
Janik Daub <daub.j@icloud.com>
2025-11-08 18:34:40 +0100

    Implements Profil

---
+++ src/main/java/com/awesome/thesis/profiles/profil/Profil.java
@@ -0,0 +1,17 @@
+package com.awesome.thesis.profiles.profil;
+
+public class Profil {
+    private String name;
+
+    public Profil(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
