84fcd5d7390f450199182aba9bd58e43162b04cf
Janik Daub <daub.j@icloud.com>
2025-11-26 12:26:11 +0100

    Refactor

--- src/test/java/com/awesome/thesis/controller/ProfilControllerTest.java
+++ src/test/java/com/awesome/thesis/controller/ProfilControllerTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
@@ -23,7 +23,7 @@ class ProfilControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     ProfilEditor editor;
 
     @Test
