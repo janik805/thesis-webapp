ad88a1c05015591c80bab60c8076b1409faad7ca
Ryota Kariya <qet71seq@hhu.de>
2025-12-05 12:04:22 +0100

    Add equals and hash method to Thema

--- src/main/java/com/awesome/thesis/logic/domain/model/themen/Thema.java
+++ src/main/java/com/awesome/thesis/logic/domain/model/themen/Thema.java
@@ -3,6 +3,7 @@ package com.awesome.thesis.logic.domain.model.themen;
 import com.awesome.thesis.logic.domain.model.links.Link;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Thema {
     private String titel;
@@ -40,4 +41,20 @@ public class Thema {
     public void removeUrl(Link link) {
         links.remove(link);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Thema thema = (Thema) o;
+        return Objects.equals(titel, thema.titel)
+                && Objects.equals(beschreibung, thema.beschreibung);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(titel, beschreibung);
+    }
+
 }
