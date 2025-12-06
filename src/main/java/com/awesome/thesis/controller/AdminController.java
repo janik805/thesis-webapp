342fd7249eb48152c3641d2b4299cdc0c4ac4fa2
Janik Daub <daub.j@icloud.com>
2025-12-06 18:26:40 +0100

    Implements first version of admin/profilEdit with change name and change kontakt

--- src/main/java/com/awesome/thesis/controller/AdminController.java
+++ src/main/java/com/awesome/thesis/controller/AdminController.java
@@ -1,12 +1,11 @@
 package com.awesome.thesis.controller;
 
 import com.awesome.thesis.logic.application.service.profiles.ProfilEditor;
+import com.awesome.thesis.logic.domain.model.profil.Profil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/admin")
@@ -18,5 +17,11 @@ public class AdminController {
     public String profilEdit(@PathVariable String id, Model model) {
         model.addAttribute("profil", editor.get(id));
         return "admin/profilEdit";
+    }
+
+    @PostMapping("profilEdit/{id}")
+    public String profilEdit(@PathVariable String id, @ModelAttribute("name") String name) {
+        editor.editName(id, name);
+        return "redirect:/admin/profilEdit/" + id;
     }
 }
