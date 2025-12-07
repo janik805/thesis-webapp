5af5e70906ba8c307756aea4dbaf1e511b81fc0c
Janik Daub <daub.j@icloud.com>
2025-12-07 15:53:17 +0100

    Implements Test for only one Aggregate Root

---
+++ src/test/java/com/awesome/thesis/architecture/AggregatTest.java
@@ -0,0 +1,17 @@
+package com.awesome.thesis.architecture;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import org.junit.jupiter.api.Disabled;
+
+import static com.awesome.thesis.rules.HaveExactlyOneAggregateRoot.HAVE_EXACTLY_ONE_AGGREGATE_ROOT;
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
+
+@AnalyzeClasses(packages = "com.awesome.thesis.logic.domain.model..")
+public class AggregatTest {
+    @ArchTest
+    static final ArchRule oneAggregatRootPerAggregate = slices()
+            .matching("..(*)..")
+            .should(HAVE_EXACTLY_ONE_AGGREGATE_ROOT);
+}
