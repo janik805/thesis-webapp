164f280c43f454de17b721d8e6a723ef3ff5c121
Janik Daub <daub.j@icloud.com>
2025-12-08 10:16:07 +0100

    Adds annotations for aggregats

---
+++ src/main/java/com/awesome/thesis/annotations/AggregatEntity.java
@@ -0,0 +1,9 @@
+package com.awesome.thesis.annotations;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+public @interface AggregatEntity {
+}
