43a10b039cd12fb41f8a145250c198f994b10694
Janik Daub <daub.j@icloud.com>
2025-12-08 10:06:44 +0100

    Refactor

--- src/test/java/com/awesome/thesis/architecture/AggregatTest.java
+++ src/test/java/com/awesome/thesis/architecture/AggregatTest.java
@@ -3,7 +3,6 @@ package com.awesome.thesis.architecture;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
-import org.junit.jupiter.api.Disabled;
 
 import static com.awesome.thesis.rules.HaveExactlyOneAggregateRoot.HAVE_EXACTLY_ONE_AGGREGATE_ROOT;
 import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
