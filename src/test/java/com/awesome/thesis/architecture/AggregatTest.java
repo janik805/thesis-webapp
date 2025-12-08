c427cee823504a3cfb413bf7255937fc1fe2914a
Janik Daub <daub.j@icloud.com>
2025-12-08 14:15:07 +0100

    Implements tests to test the rules of aggregates

--- src/test/java/com/awesome/thesis/architecture/AggregatTest.java
+++ src/test/java/com/awesome/thesis/architecture/AggregatTest.java
@@ -1,10 +1,14 @@
 package com.awesome.thesis.architecture;
 
+import com.awesome.thesis.annotations.AggregateEntity;
+import com.awesome.thesis.annotations.AggregateRoot;
+import com.awesome.thesis.annotations.AggregateValue;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 
 import static com.awesome.thesis.rules.HaveExactlyOneAggregateRoot.HAVE_EXACTLY_ONE_AGGREGATE_ROOT;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
 
 @AnalyzeClasses(packages = "com.awesome.thesis.logic.domain.model..")
@@ -13,4 +17,27 @@ public class AggregatTest {
     static final ArchRule oneAggregatRootPerAggregate = slices()
             .matching("..(*)..")
             .should(HAVE_EXACTLY_ONE_AGGREGATE_ROOT);
+
+    @ArchTest
+    static final ArchRule allEntitiesExceptRootArePrivate = classes()
+            .that()
+            .areAnnotatedWith(AggregateEntity.class)
+            .and()
+            .areNotAnnotatedWith(AggregateRoot.class)
+            .should()
+            .notBePublic();
+
+    @ArchTest
+    static final ArchRule allClassesNeedToBeValueOrEntity = classes()
+            .should()
+            .beAnnotatedWith(AggregateValue.class)
+            .orShould()
+            .beAnnotatedWith(AggregateEntity.class);
+
+    @ArchTest
+    static final ArchRule aggregateRootShouldBePublic = classes()
+            .that()
+            .areAnnotatedWith(AggregateRoot.class)
+            .should()
+            .bePublic();
 }
