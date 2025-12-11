2de5ec523567564bf9191e27130c64d3904cdaf5
Ryota Kariya <qet71seq@hhu.de>
2025-12-11 20:44:04 +0100

    Add functionality to view single Thema objects with all their info

--- src/main/java/com/awesome/thesis/controller/ThemaController.java
+++ src/main/java/com/awesome/thesis/controller/ThemaController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ThemaController {
@@ -16,5 +17,11 @@ public class ThemaController {
     public String themenListe(Model model) {
         model.addAttribute("themenListe", thema.getAll());
         return "themen/themenListe";
+    }
+
+    @GetMapping("/thema/{id}")
+    public String thema(@PathVariable("id") String id, Model model) {
+        model.addAttribute("thema", thema.getThema(id));
+        return "themen/thema";
     }
 }
