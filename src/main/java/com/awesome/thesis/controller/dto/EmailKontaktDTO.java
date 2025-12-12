4529d8b853fea990ef66f23d76a9ed3612fc563b
Janik Daub <daub.j@icloud.com>
2025-12-12 13:30:10 +0100

    Adds NotBlank validation for Email

--- src/main/java/com/awesome/thesis/controller/dto/EmailKontaktDTO.java
+++ src/main/java/com/awesome/thesis/controller/dto/EmailKontaktDTO.java
@@ -1,6 +1,8 @@
 package com.awesome.thesis.controller.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
-public record EmailKontaktDTO(String label, @Email String wert) {
-}
+public record EmailKontaktDTO (String label,
+    @NotBlank(message = "Email darf nicht leer sein")
+    @Email(message = "Bitte geben Sie eine gültige Email Adresse ein") String wert) {}
