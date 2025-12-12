f3185f8d743eb41781e9363c5bfad9a8ee80aadd
Ryota Kariya <qet71seq@hhu.de>
2025-12-12 17:53:59 +0100

    Add a method to reach the edit html page when an id is already given

--- src/main/java/com/awesome/thesis/controller/admin/ThemaEditorController.java
+++ src/main/java/com/awesome/thesis/controller/admin/ThemaEditorController.java
@@ -1,4 +1,21 @@
 package com.awesome.thesis.controller.admin;
 
+import com.awesome.thesis.logic.application.service.themen.ThemaEditor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class ThemaEditorController {
+
+    @Autowired
+    ThemaEditor editor;
+
+    @GetMapping("/editThema/{id}")
+    public String editThema(@PathVariable("id")String id, Model model) {
+        model.addAttribute("thema", editor.getThema(id));
+        return "admin/themaEdit";
+    }
 }
--- src/test/java/com/awesome/thesis/controller/ThemaEditorControllerTest.java
+++ src/test/java/com/awesome/thesis/controller/ThemaEditorControllerTest.java
@@ -1,4 +1,30 @@
 package com.awesome.thesis.controller;
 
+import com.awesome.thesis.controller.admin.ThemaEditorController;
+import com.awesome.thesis.logic.application.service.themen.ThemaEditor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ThemaEditorController.class)
 public class ThemaEditorControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @MockitoBean
+    ThemaEditor themaEditor;
+
+    @Test
+    @DisplayName("Tests that admin/themaEdit is rechable")
+    void test_1() throws Exception {
+        mvc.perform(get("/editThema/propra"))
+                .andExpect(status().isOk());
+    }
 }
