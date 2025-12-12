dc3b8f75f429d9d7a9a157946056ec126fa09e94
Ryota Kariya <qet71seq@hhu.de>
2025-12-12 17:46:38 +0100

    Make ThemaDTO a record with fields to match the Thema object

--- src/main/java/com/awesome/thesis/controller/dto/ThemaDTO.java
+++ src/main/java/com/awesome/thesis/controller/dto/ThemaDTO.java
@@ -1,4 +1,10 @@
 package com.awesome.thesis.controller.dto;
 
-public class ThemaDTO {
-}
+import com.awesome.thesis.logic.domain.model.links.Link;
+
+import java.util.List;
+
+public record ThemaDTO(
+        String titel,
+        String beschreibung,
+        List<Link> urls) {}
