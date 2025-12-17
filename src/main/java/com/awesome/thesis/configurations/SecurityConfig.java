6c0c5a34fa7ef0192995afe08e60893f7ea8fb4e
Janik Daub <daub.j@icloud.com>
2025-12-17 11:13:23 +0100

    Refactor

--- src/main/java/com/awesome/thesis/configurations/SecurityConfig.java
+++ src/main/java/com/awesome/thesis/configurations/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.awesome.thesis.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,10 +14,7 @@ public class SecurityConfig {
                         configurer -> configurer
                                 .anyRequest().authenticated()
                 )
-                .oauth2Login(config ->
-                        config.userInfoEndpoint(
-                                info -> info.userService(appUserService))
-                        );
+                .oauth2Login(Customizer.withDefaults());
         return chainBuilder.build();
     }
 }
