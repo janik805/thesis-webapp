c16d2a981ae375091b7d61742133b2b43a599a18
Janik Daub <daub.j@icloud.com>
2026-01-10 17:46:35 +0100

    Adds option to enter Interessen and refactors matching-page

--- src/main/java/com/awesome/thesis/controller/MatchingController.java
+++ src/main/java/com/awesome/thesis/controller/MatchingController.java
@@ -1,18 +1,27 @@
 package com.awesome.thesis.controller;
 
+import com.awesome.thesis.logic.application.service.fachgebiete.FachgebieteEditor;
 import com.awesome.thesis.logic.application.service.profiles.ProfilEditor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Set;
 
 @Controller
 public class MatchingController {
     @Autowired
     ProfilEditor profilEditor;
 
+    @Autowired
+    FachgebieteEditor fachgebieteEditor;
+
     @GetMapping("/matching")
-    public String matching(Model model) throws Exception {
+    public String matching(Model model, @RequestParam(required = false) Set<String> interessen) {
+        model.addAttribute("interessen", interessen);
+        model.addAttribute("fachgebiete", fachgebieteEditor.getAll());
         model.addAttribute("profile", profilEditor.getAll());
         return "matching";
     }
