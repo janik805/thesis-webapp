03cba744e4740b14da3b9b006d9603c0b014c57d
Janik Daub <daub.j@icloud.com>
2026-01-14 17:04:52 +0100

    Adds h2 database for tests and repairs main test

--- src/test/java/com/awesome/thesis/ThesisApplicationTests.java
+++ src/test/java/com/awesome/thesis/ThesisApplicationTests.java
@@ -1,9 +1,11 @@
 package com.awesome.thesis;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class ThesisApplicationTests {
 
 	@Test
