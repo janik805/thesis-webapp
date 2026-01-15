4e90211d09770973c7e03d765f8c7530149043df
Janik Daub <daub.j@icloud.com>
2026-01-15 10:34:20 +0100

    Versions of Database profil and fachgebiete not in right package

---
+++ V1__init_profil.sql
@@ -0,0 +1,45 @@
+create table profil
+(
+    id integer primary key,
+    name varchar(50) not null
+);
+
+create table profil_kontakte
+(
+    id serial primary key,
+    profil integer not null references profil (id),
+    label varchar(100),
+    wert varchar(100) not null,
+    kontaktart varchar(50)  not null,
+    unique (profil, label, wert, kontaktart)
+);
+
+create table profil_fachgebiete
+(
+    profil integer references profil (id),
+    fachgebiet varchar(50) not null,
+    primary key (profil, fachgebiet)
+);
+
+create table profil_links
+(
+    profil integer references profil (id),
+    url varchar(150) not null,
+    beschreibung varchar(500),
+    primary key (profil, url, beschreibung)
+);
+
+create table profil_thema_value
+(
+    id varchar(150) primary key,
+    profil integer not null references profil (id),
+    name varchar(50)
+);
+
+create table profil_datei_value
+(
+    id varchar(150) primary key,
+    profil integer not null references profil (id),
+    name varchar(50),
+    beschreibung varchar(200)
+);
---
+++ V2__init_fachgebiete.sql
@@ -0,0 +1,4 @@
+create table fachgebiete
+(
+    fachgebiet varchar(100) primary key
+);
