582761778e2095e6df53404dc9cc9db25e1ce1fb
Janik Daub <daub.j@icloud.com>
2026-01-15 10:33:39 +0100

    Fachgebiete DB Repo

---
+++ src/main/java/com/awesome/thesis/persistence/fachgebiete/FachgebieteDBRepository.java
@@ -0,0 +1,6 @@
+package com.awesome.thesis.persistence.fachgebiete;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface FachgebieteDBRepository extends CrudRepository<String, String> {
+}
