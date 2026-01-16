0b43620527ec551408b2208f70ace5c7cfe5b166
Janik Daub <daub.j@icloud.com>
2026-01-16 12:01:34 +0100

    Adds methods to Crud

--- src/main/java/com/awesome/thesis/persistence/fachgebiete/FachgebieteDBRepository.java
+++ src/main/java/com/awesome/thesis/persistence/fachgebiete/FachgebieteDBRepository.java
@@ -1,7 +1,19 @@
 package com.awesome.thesis.persistence.fachgebiete;
 
 import com.awesome.thesis.logic.domain.model.fachgebiete.Fachgebiet;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
+
+import java.util.Set;
 
 public interface FachgebieteDBRepository extends CrudRepository<Fachgebiet, String> {
+    @NonNull
+    Set<Fachgebiet> findAll();
+
+    @Modifying
+    @Query("insert into fachgebiet values (:name)")
+    void save(@Param("name") String name);
 }
