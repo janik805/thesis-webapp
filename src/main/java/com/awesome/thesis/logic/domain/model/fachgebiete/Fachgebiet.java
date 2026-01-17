99a6b4e1db78e5cf3363a946eebcb6248d1dd776
Janik Daub <daub.j@icloud.com>
2026-01-17 19:05:48 +0100

    Adds equals and hasCode

--- src/main/java/com/awesome/thesis/logic/domain/model/fachgebiete/Fachgebiet.java
+++ src/main/java/com/awesome/thesis/logic/domain/model/fachgebiete/Fachgebiet.java
@@ -3,6 +3,8 @@ package com.awesome.thesis.logic.domain.model.fachgebiete;
 import com.awesome.thesis.annotations.AggregateRoot;
 import org.springframework.data.annotation.Id;
 
+import java.util.Objects;
+
 @AggregateRoot
 public class Fachgebiet {
     @Id
@@ -14,5 +16,17 @@ public class Fachgebiet {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Fachgebiet that = (Fachgebiet) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
     }
 }
