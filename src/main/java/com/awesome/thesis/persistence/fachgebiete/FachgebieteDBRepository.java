63d600357e1666d1f1a72e161968e875616d4c29
Janik Daub <daub.j@icloud.com>
2026-01-19 19:53:38 +0100

    Code Cleanup

--- src/main/java/com/awesome/thesis/persistence/fachgebiete/FachgebieteDBRepository.java
+++ src/main/java/com/awesome/thesis/persistence/fachgebiete/FachgebieteDBRepository.java
@@ -1,6 +1,5 @@
 package com.awesome.thesis.persistence.fachgebiete;
 
-import com.awesome.thesis.logic.domain.model.fachgebiete.Fachgebiet;
 import com.awesome.thesis.persistence.fachgebiete.dto.FachgebietDTO;
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
