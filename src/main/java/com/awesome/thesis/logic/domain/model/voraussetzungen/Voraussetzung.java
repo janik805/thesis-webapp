61ecd122933ba00984788cd444c446a04f739202
Ryota Kariya <qet71seq@hhu.de>
2026-01-20 14:45:25 +0100

    Implements Google Standard in domain.model.voraussetzungen

--- src/main/java/com/awesome/thesis/logic/domain/model/voraussetzungen/Voraussetzung.java
+++ src/main/java/com/awesome/thesis/logic/domain/model/voraussetzungen/Voraussetzung.java
@@ -1,31 +1,43 @@
 package com.awesome.thesis.logic.domain.model.voraussetzungen;
 
 import com.awesome.thesis.annotations.AggregateRoot;
+import java.util.Objects;
 import org.springframework.data.annotation.Id;
 
-import java.util.Objects;
-
+/**
+ * Aggregate Root der Voraussetzung.
+ */
 @AggregateRoot
 public class Voraussetzung {
-    @Id
-    private final String voraussetzung;
 
-    public Voraussetzung(String voraussetzung) {
-        this.voraussetzung = voraussetzung;
-    }
+  @Id
+  private final String voraussetzung;
 
-    public String getVoraussetzung() {
-        return voraussetzung;
-    }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Voraussetzung that)) return false;
-        return Objects.equals(voraussetzung, that.voraussetzung);
-    }
+  /**
+   * Initialisiert die Voraussetzung.
+   * @param voraussetzung Eingabe als String.
+   */
+  public Voraussetzung(String voraussetzung) {
+    this.voraussetzung = voraussetzung;
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(voraussetzung);
+  public String getVoraussetzung() {
+    return voraussetzung;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
+    if (!(o instanceof Voraussetzung that)) {
+      return false;
+    }
+    return Objects.equals(voraussetzung, that.voraussetzung);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(voraussetzung);
+  }
 }
