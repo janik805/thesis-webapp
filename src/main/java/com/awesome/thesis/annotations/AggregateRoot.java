5a39ba3b96e8ace186604df7acfcc224e63204e9
Janik Daub <daub.j@icloud.com>
2026-01-21 13:51:40 +0100

    annotations follows Google Standard

--- src/main/java/com/awesome/thesis/annotations/AggregateRoot.java
+++ src/main/java/com/awesome/thesis/annotations/AggregateRoot.java
@@ -1,7 +1,14 @@
 package com.awesome.thesis.annotations;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+/**
+ * Annotation für AggregatRoot. Diese Klasse ist mit {@link AggregateEntity} annotiert.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Documented
