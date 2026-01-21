36704220d54e243b4a43eee485092f1f7bf374cd
Ole Marschik <ole.marschik@hhu.de>
2026-01-21 14:28:08 +0100

    checkstyle fixed DateiTypPruefer

--- src/main/java/com/awesome/thesis/logic/application/service/files/DateiTypPruefer.java
+++ src/main/java/com/awesome/thesis/logic/application/service/files/DateiTypPruefer.java
@@ -1,20 +1,29 @@
 package com.awesome.thesis.logic.application.service.files;
 
-import org.springframework.web.multipart.MultipartFile;
 import java.util.Set;
+import org.springframework.web.multipart.MultipartFile;
 
+/**
+ * Klasse, die prüft, ob eine Datei ein erlaubtes Dateiformat besitzt.
+ */
 public class DateiTypPruefer {
-    private static final Set<String> ERLAUBTE_DATEIENDUNGEN = Set.of("zip", "pdf", "md");
+  private static final Set<String> ERLAUBTE_DATEIENDUNGEN
+      = Set.of("zip", "pdf", "md");
 
-    public static void verify(MultipartFile datei) {
-        String dateiName = datei.getOriginalFilename();
-        boolean erlaubt = ERLAUBTE_DATEIENDUNGEN.stream()
-                .anyMatch(dateiName::endsWith);
+  /**
+   * Methode, die Datei-Endung mit den erlaubten Endungen vergleicht.
+   *
+   * @param datei Datei.
+   */
+  public static void verify(MultipartFile datei) {
+    String dateiName = datei.getOriginalFilename();
+    boolean erlaubt = ERLAUBTE_DATEIENDUNGEN.stream()
+        .anyMatch(dateiName::endsWith);
 
-        if (!erlaubt) {
-            throw new IllegalArgumentException(
-                    "Dateityp nicht erlaubt: " + dateiName);
-        }
-
+    if (!erlaubt) {
+      throw new IllegalArgumentException(
+          "Dateityp nicht erlaubt: " + dateiName);
     }
+
+  }
 }
