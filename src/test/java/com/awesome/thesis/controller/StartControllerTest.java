a71b7eac816904200590035b1cea183827d695d3
Janik Daub <daub.j@icloud.com>
2026-01-21 14:33:30 +0100

    Google Standard

--- src/test/java/com/awesome/thesis/controller/StartControllerTest.java
+++ src/test/java/com/awesome/thesis/controller/StartControllerTest.java
@@ -1,9 +1,11 @@
 package com.awesome.thesis.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.awesome.thesis.configurations.AppUserService;
 import com.awesome.thesis.configurations.MethodSecurityConfig;
 import com.awesome.thesis.configurations.SecurityConfig;
-import com.awesome.thesis.controller.admin.AdminProfilCreator;
 import com.awesome.thesis.logic.application.service.profiles.ProfilEditor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,23 +16,20 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @Import({SecurityConfig.class, MethodSecurityConfig.class, AppUserService.class})
 @WebMvcTest(StartController.class)
 class StartControllerTest {
-    @Autowired
-    MockMvc mockMvc;
-
-    @MockitoBean
-    ProfilEditor editor;
-
-    @Test
-    @WithMockUser()
-    @DisplayName("Ein unangemeldeter Nutzer kann die Start Seite aufrufen")
-    void get_createProfil() throws Exception {
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk());
-    }
+  @Autowired
+  MockMvc mockMvc;
+  
+  @MockitoBean
+  ProfilEditor editor;
+  
+  @Test
+  @WithMockUser()
+  @DisplayName("Ein unangemeldeter Nutzer kann die Start Seite aufrufen")
+  void get_createProfil() throws Exception {
+    mockMvc.perform(get("/"))
+        .andExpect(status().isOk());
+  }
 }
