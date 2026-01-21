4dcb233fdf01d62fc258b8288dbd9c60088bd23f
Ryota Kariya <qet71seq@hhu.de>
2026-01-21 14:31:36 +0100

    Implements Google standards in ThemaApplication

--- src/main/java/com/awesome/thesis/ThesisApplication.java
+++ src/main/java/com/awesome/thesis/ThesisApplication.java
@@ -1,34 +1,16 @@
 package com.awesome.thesis;
 
-import com.awesome.thesis.logic.application.service.profiles.ProfilEditor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
-//public class ThesisApplication {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(ThesisApplication.class, args);
-//	}
-//
-//}
-
-
+/**
+ * Startet die Applikation.
+ */
 @SpringBootApplication
-public class ThesisApplication implements CommandLineRunner {
-	@Autowired
-	ProfilEditor profilEditor;
+public class ThesisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ThesisApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ThesisApplication.class, args);
+  }
 
-	@Override
-	public void run(String ... args) throws Exception{
-		profilEditor.create(182077829,"Janik Daub");
-		profilEditor.create(180645494, "Ryota Kariya");
-		profilEditor.create(181595941, "Ole Marschik");
-	}
 }
